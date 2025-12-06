734a96988784751e69eeabdc6cbb8317cedf32af
kkhhjjoo <khjoo1203@gmail.com>
2025-12-06 23:13:27 +0900

    jQuery 사용법 간단정리

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const navToggler = document.querySelector('.navbar-toggler');
 const listGroup = document.querySelector('.list-group');
 
 navToggler.addEventListener('click', () => listGroup.classList.toggle('show'));
+
+
+// const hello = document.querySelector('.hello');
+// hello.textContent = '바보';
+// hello.innerHTML = '바보';
+
+// hello.style.fontSize = '32px';
+// hello.style.color = 'red';
