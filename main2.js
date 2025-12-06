7d1320be7c631398309b83c2bd7ee2864b2f8f1e
kkhhjjoo <khjoo1203@gmail.com>
2025-12-06 23:56:44 +0900

    모달창 중간 저장

---
+++ main2.js
@@ -0,0 +1,18 @@
+const navToggler = document.querySelector('.navbar-toggler');
+const listGroup = document.querySelector('.list-group');
+
+navToggler.addEventListener('click', () => listGroup.classList.toggle('show'));
+
+
+// const login = document.querySelector('.login');
+// const blackBg = document.querySelector('.black-bg');
+// const close = document.querySelector('#close');
+
+// login.addEventListener('click', function () {
+//   blackBg.classList.add('show-modal');
+// });
+
+// close.addEventListener('click', function () {
+//   blackBg.classList.remove('show-modal');
+// });
+
