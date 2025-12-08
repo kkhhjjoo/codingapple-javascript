5adeb5cba94cdb1c820f0433819bacd11358e440
kkhhjjoo <khjoo1203@gmail.com>
2025-12-08 21:40:26 +0900

    input, change 이벤트와 and, or 연산자

--- main2.js
+++ main2.js
@@ -1,7 +1,10 @@
 const navToggler = document.querySelector('.navbar-toggler');
 const listGroup = document.querySelector('.list-group');
 
-navToggler.addEventListener('click', () => listGroup.classList.toggle('show'));
+// navToggler.addEventListener('click', () => listGroup.classList.toggle('show'));
+
+
+
 
 
 // const login = document.querySelector('.login');
